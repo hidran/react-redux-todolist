e8bb74ccd48a56094e1cbf85b47b92992eae8923
Hidran Arias <hidran@gmail.com>
2021-08-10 00:48:26 +0200

    Creates react app with redux toolkit

--- src/App.js
+++ src/App.js
@@ -1,56 +1,43 @@
-import React from 'react';
-import logo from './logo.svg';
-import { Counter } from './features/counter/Counter';
+import React,{useState, useEffect} from 'react';
+
 import './App.css';
 
+const initTodos = [
+  {
+    name: 'Call my mum',
+    dueDate: new Date().toLocaleDateString(),
+    user_id : 1
+  },
+   {
+    name: 'Go to school',
+    dueDate: new Date().toLocaleDateString(),
+    user_id : 1
+  },
+    {
+    name: 'Do my homework',
+    dueDate: new Date().toLocaleDateString(),
+    user_id : 1
+  }
+
+];
+
 function App() {
+  const [todos, setTodos] = useState([]);
+   useEffect(() => {
+     setTodos(initTodos);
+     return () => {
+      
+     }
+   }, [todos])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Counter />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <span>
-          <span>Learn </span>
-          <a
-            className="App-link"
-            href="https://reactjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux-toolkit.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux Toolkit
-          </a>
-          ,<span> and </span>
-          <a
-            className="App-link"
-            href="https://react-redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React Redux
-          </a>
-        </span>
-      </header>
+    <div className="App container-fluid">
+      <h1>MY TODO LIST</h1>
+      <ul className="list-group list-group-flush">
+        {
+          todos.map(todo => <li key={todo.name} className="list-group-item">{ todo.name}</li>)
+        }
+      </ul>
+     
     </div>
   );
 }
