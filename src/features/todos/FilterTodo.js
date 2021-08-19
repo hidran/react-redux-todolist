1af3f68f667f9a06c0e233d27e56c2555c48690e
Hidran Arias <hidran@gmail.com>
2021-08-20 00:00:38 +0200

    adds class management for filter buttons

--- src/features/todos/FilterTodo.js
+++ src/features/todos/FilterTodo.js
@@ -1,19 +1,52 @@
 
 
-const FilterTodo = ({onFilter,filter}) => {
+const FilterTodo = ({ onFilter, filter }) => {
+    
+    let { btnClassAll, btnClassTodo, btnClassCompleted } = getBtnClass(filter);
     return (
-        <div className="row d-flex">
+        <div className="row d-flex btn-d">
             <div className="col-sm-4">
-                <button disabled = {filter ==='ALL'} onClick={() => onFilter('ALL')} className ="btn btn-outline-info">ALL</button>
+                <button disabled={filter === 'ALL'}
+                    onClick={() => onFilter('ALL')}
+                    className={btnClassAll}>ALL
+                </button>
             </div>
             <div className="col-sm-4">
-                <button  disabled = {filter ==='TODO'}  onClick = {onFilter.bind(null, 'TODO')} className ="btn btn-outline-info">TO DO</button>
+                <button disabled={filter === 'TODO'}
+                    onClick={onFilter.bind(null, 'TODO')}
+                    className={btnClassTodo}>T
+                    O DO
+                </button>
             </div>
             <div className="col-sm-4">
-                <button  disabled = {filter ==='COMPLETED'}   onClick = {onFilter.bind(null, 'COMPLETED')} className ="btn btn-outline-info">COMPLETED</button>
+                <button disabled={filter === 'COMPLETED'}
+                    onClick={onFilter.bind(null, 'COMPLETED')}
+                    className={btnClassCompleted}>
+                    COMPLETED
+                </button>
             </div>
         </div>
     )
 }
 
 export default FilterTodo;
+function getBtnClass(filter) {
+    let btnClassAll = 'btn w-100 btn-outline-success';
+    let btnClassTodo = 'btn w-100 btn-outline-success';
+    let btnClassCompleted = 'btn w-100 btn-outline-success';
+    switch (filter) {
+        case 'ALL':
+            btnClassAll = btnClassAll.replace('btn-outline-success', 'btn-outline-info');
+            break;
+        case 'TODO':
+            btnClassTodo = btnClassTodo.replace('btn-outline-success', 'btn-outline-info');
+            break;
+        case 'COMPLETED':
+            btnClassCompleted = btnClassCompleted.replace('btn-outline-success', 'btn-outline-info');
+            break;
+        default:
+            break;
+    }
+    return { btnClassAll, btnClassTodo, btnClassCompleted };
+}
+
